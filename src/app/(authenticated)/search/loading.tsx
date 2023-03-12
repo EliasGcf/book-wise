0b60218b0d118a40dcee3d5da87045f1af5f9a48
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-03-12 15:51:11 -0300

    chore: add loading pages

---
+++ src/app/(authenticated)/search/loading.tsx
@@ -0,0 +1,9 @@
+import { CircleNotch } from '@ui/icons';
+
+export default function Loading() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <CircleNotch size={32} weight="bold" className="animate-spin text-purple-01" />
+    </div>
+  );
+}
