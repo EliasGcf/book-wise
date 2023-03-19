722985614eca2175930eb5ca9eb768d653ea7829
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-03-19 09:29:57 -0300

    style: fix search header responsive

--- src/app/(authenticated)/search/page.tsx
+++ src/app/(authenticated)/search/page.tsx
@@ -9,7 +9,7 @@ import { Input } from '@components/Form/Input';
 export default function Search() {
   return (
     <div className="flex flex-col overflow-hidden">
-      <header className="flex items-center justify-between gap-4">
+      <header className="flex flex-col justify-between gap-4 md:flex-row md:items-center">
         <div className="flex items-center gap-3">
           <Binoculars size={32} className="text-green-01" />
           <Title size="lg" className="text-gray-01">
