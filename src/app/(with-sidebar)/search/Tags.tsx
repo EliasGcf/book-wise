ca16ec9caf04c36ab582d5376cf5e7f933c7ab3a
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-03-28 20:08:29 -0300

    style: tags focus

--- src/app/(with-sidebar)/search/Tags.tsx
+++ src/app/(with-sidebar)/search/Tags.tsx
@@ -44,7 +44,7 @@ export function Tags() {
       {TAGS.map((tag) => (
         <ToggleGroup.Item
           key={tag.value}
-          className="mr-3 mt-5 whitespace-nowrap rounded-full border border-purple-01 px-4 py-1 text-purple-01 transition-colors hover:border-purple-01 hover:bg-purple-02 hover:text-gray-01 data-[state=on]:border-purple-02 data-[state=on]:bg-purple-02 data-[state=on]:text-gray-01"
+          className="mr-3 mt-5 whitespace-nowrap rounded-full border border-purple-01 px-4 py-1 text-purple-01 outline-none transition-colors hover:border-purple-01 hover:bg-purple-02 hover:text-gray-01 focus:bg-purple-02 focus:text-gray-01 data-[state=on]:border-purple-02 data-[state=on]:bg-purple-02 data-[state=on]:text-gray-01"
           value={tag.value}
         >
           {tag.title}
