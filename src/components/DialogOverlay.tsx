ac56873f7bd3520d47e7e6d6314d06fdcc87035e
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-05-06 16:05:17 -0300

    fix: dialog overlay forwardRef

--- src/components/DialogOverlay.tsx
+++ src/components/DialogOverlay.tsx
@@ -1,7 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { forwardRef } from 'react';
 
-export function DialogOverlay() {
+export const DialogOverlay = forwardRef<HTMLDivElement, Dialog.DialogOverlayProps>(() => {
   return (
     <Dialog.Overlay className="fixed inset-0 bg-black/60 data-[state=open]:animate-in data-[state=open]:fade-in" />
   );
-}
+});
